ad0ce84e3c5163bb01a4967071c54439f92e79c5
Alperen Sah Abursum <85456369+alperen-cpu@users.noreply.github.com>
2023-01-06 12:21:22 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 bold=$(tput bold)
 normal=$(tput sgr0)
-echo "#--------------------${bold}Welcome $USER${normal}--------------------#"
+echo "#--------------------${bold}Welcome $USER ${normal}--------------------#"
 echo "${bold}Date:${normal}";date
 echo "${bold}Server uptime:${normal}" && uptime -p
-echo "${bold}Last Logins:${normal}" && last $USER | head -n 5
+echo "${bold}Operating system:${normal}";cat /etc/os-release | grep '^VERSION' /etc/os-release | grep -E '^(VERSION|NAME)=' /etc/os-release
+echo "${bold}Last Logins:${normal}" && last $USER | head -n 3;last $USER | head -n 3 >> /tmp/lastlogin.txt
+echo "Sending email..."
+echo -e "\nThe user $USER is logged into the $HOSTNAME server.\nLast Login:\n`cat /tmp/lastlogin.txt`" | msmtp -a mail example@example.com
 echo "--------------------"
 echo "${bold}Disk Info:${normal}"
 df -h -t ext4 | awk '{print $2 " " $3 " " $4}'
