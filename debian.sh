a0c854cd5ca6252c7518aacb624f46602515f53f
Alperen Şah Aburşum <85456369+alperen-cpu@users.noreply.github.com>
2023-12-06 14:22:13 +0300

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -1,20 +1,42 @@
 #!/bin/bash
+
 USER=$(whoami)
 rm -rf /tmp/lastlogin.txt
-bold=$(tput bold)
-normal=$(tput sgr0)
-echo "#--------------------${bold}Welcome $USER ${normal}--------------------#"
-echo "${bold}Date:${normal}";date
-echo "${bold}Server uptime:${normal}" && uptime -p
-echo "${bold}Operating system:${normal}";cat /etc/os-release | grep '^VERSION' /etc/os-release | grep -E '^(VERSION|NAME)=' /etc/os-release
-echo "${bold}Last Logins:${normal}" && last $USER | head -n 3;last $USER | head -n 3 >> /tmp/lastlogin.txt
-echo "Sending email..."
+echo "███████████████████████████████████████████████████████████████"
+echo "█                                                             █"
+echo "█               ${bold}Welcome to My Server${normal}                          █"
+echo "█                                                             █"
+echo "███████████████████████████████████████████████████████████████"
+echo ""
+echo "  Date: $(date)"
+echo "  Server uptime: $(uptime -p)"
+echo ""
+echo "  Operating System:"
+echo "    Distribution: $(cat /etc/os-release | grep '^NAME' | cut -d '"' -f2)"
+echo "    Version: $(cat /etc/os-release | grep '^VERSION_ID' | cut -d '"' -f2)"
+echo ""
+echo "  Last Logins:"
+last $USER | head -n 3
+echo ""
+last $USER | head -n 3 >> /tmp/lastlogin.txt
 echo -e "Subject: SSH Login $HOSTNAME\r\n\r\nThe user $USER is logged into the $HOSTNAME server.\nLast Login:\n`cat /tmp/lastlogin.txt`" | msmtp -a mail example@example.com
-echo "--------------------"
-echo "${bold}Disk Info:${normal}"
-df -h -t ext4 | awk '{print $2 " " $3 " " $4}'
-echo "${bold}Memory Info:${normal}"
-cat /proc/meminfo | numfmt --field 2 --from-unit=Ki --to-unit=Mi | sed 's/ kB/M/g'| grep -e "MemFree" -we "MemTotal"
-echo "--------------------"
-echo "${bold}CPU Info:${normal}"
-echo "CPU Usage: "$[100-$(vmstat 1 2|tail -1|awk '{print $15}')]"%"
+echo "  Sending email..."
+echo "  Email sent."
+echo ""
+echo "  Disk Info:"
+df / -H --output=source,size,used,avail
+echo ""
+echo "  Memory Info:"
+awk '$3=="kB" {
+    if ($2 > 1024^2) {
+        $2 = $2 / 1024^2;
+        $3 = "GB";
+    } else if ($2 > 1024) {
+        $2 = $2 / 1024;
+        $3 = "MB";
+    }
+} 1' /proc/meminfo | grep -e "MemTotal" -e "MemFree" -e "MemAvailable"
+echo ""
+echo "  CPU Info:"
+cpu_usage=$(vmstat 1 2 | tail -1 | awk '{print 100 - $15}')
+echo "  CPU Usage: ${cpu_usage}%"
